c8a353860db71508ba031ca65a913c34746042d4
BenCoke <bencoke49@gmail.com>
2022-07-26 20:02:51 +0100

    Delete test.js

--- public/scripts/test.js
+++
@@ -1,6 +0,0 @@
-//import User, { printName, printAge } from '../../node_modules/@scottish-government/pattern-library/src/users.js'
-import User, { printName, printAge } from '/src/users.js'
-
-const user = new User('Bob', 11)
-console.log(user)
-printName(user)
